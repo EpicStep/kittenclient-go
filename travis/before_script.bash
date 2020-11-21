bf9676a2fb1386491cc58f54fa75d5353a46a042
Konstantin Gvencadze <k.gvencadze@icloud.com>
2020-11-21 17:28:14 +0300

    add go get kittenhouse

--- travis/before_script.bash
+++ travis/before_script.bash
@@ -4,3 +4,8 @@ clickhouse-client -q "CREATE TABLE default.internal_logs ( date Date DEFAULT toD
 clickhouse-client -q "CREATE TABLE default.internal_logs_buffer AS default.internal_logs ENGINE = Buffer(default, internal_logs, 2, 10, 10, 10000000, 10000000, 100000000, 100000000);"
 clickhouse-client -q "CREATE TABLE default.daemon_heartbeat ( date Date DEFAULT toDate(time), time DateTime, daemon String, server String, port Int32, build_version String, build_commit String, config_ts DateTime, config_hash String, memory_used_bytes Int64, cpu_user_avg Float32, cpu_sys_avg Float32, server_time DateTime DEFAULT now() ) ENGINE = ReplacingMergeTree(date, (daemon, server, port), 8192, server_time);"
 clickhouse-client -q "CREATE TABLE default.daemon_heartbeat_buffer AS default.daemon_heartbeat ENGINE = Buffer(default, daemon_heartbeat, 2, 15, 15, 10000000, 10000000, 100000000, 100000000);"
+
+go get github.com/VKCOM/kittenhouse
+cd $GOPATH/go/src/vkcom/kittenhouse
+go build main.go
+./main -u="" -g=""
